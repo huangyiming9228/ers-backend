dec0545ad9618ed38b8a580a274f421a717a9461
huangyiming9228 <764520351@qq.com>
2019-05-21 22:29:04 +0800

    添加设备维护接口

--- application/api/controller/EtManage.php
+++ application/api/controller/EtManage.php
@@ -26,6 +26,16 @@ class EtManage extends Controller {
     return $url;
   }
 
+  public function getAllAreas() {
+    $data = Db::table('areas')->select();
+    return $this->formatData('ok', $data);
+  }
+
+  public function getAllRooms($id) {
+    $data = Db::table('rooms')->where('area_id', $id)->select();
+    return $this->formatData('ok', $data);
+  }
+
   public function getEtCheckoutList() {
     $params = request()->param();
     $data = Db::table('et_checkout')
@@ -152,6 +162,68 @@ class EtManage extends Controller {
       $res = $data;
     }
     return $this->formatData('ok', $res);
+  }
+
+  public function getFaultHandingList() {
+    $params = request()->param();
+    $conditions = [];
+    if ($params['area_id']) $conditions['area_id']  = $params['area_id'];
+    if ($params['room_id']) $conditions['room_id']  = $params['room_id'];
+    if ($params['start_time'] && $params['end_time']) {
+      $conditions['submit_time']  = ['between', [$params['start_time'], $params['end_time']]];
+    }
+    $data = Db::table('faulthanding_list')->where($conditions)->order('submit_time', 'desc')->select();
+    foreach ($data as $key => $value) {
+      // 获取区域、教室信息
+      $room_info = Db::table('rooms')->where('id', $value['room_id'])->find();
+      $area_info = Db::table('areas')->where('id', $value['area_id'])->find();
+      $data[$key]['area_name'] = $area_info['area_name'];
+      $data[$key]['room_name'] = $room_info['room_name'];
+
+      // 获取设备信息
+      $et_info = Db::table('equipments')->where('id', $value['equipment_id'])->find();
+      $et_info['et_type'] = Db::table('equipment_class')->where('id', $et_info['class_id'])->value('equipment_name');
+      $data[$key]['et_info'] = $et_info;
+
+      // 获取故障列表
+      $fault_list = Db::table('equipment_fault')->where('faulthanding_id', $value['id'])->select();
+      foreach ($fault_list as $sub_key => $sub_value) {
+        $fault_list[$sub_key]['fault_name'] = Db::table('faults_class')->where('id', $sub_value['fault_id'])->value('fault_name');
+      }
+      $data[$key]['fault_list'] = $fault_list;
+    }
+    return $this->formatData('ok', $data);
+  }
+
+  public function getTechHandingList() {
+    $params = request()->param();
+    $conditions = [];
+    if ($params['area_id']) $conditions['area_id']  = $params['area_id'];
+    if ($params['room_id']) $conditions['room_id']  = $params['room_id'];
+    if ($params['start_time'] && $params['end_time']) {
+      $conditions['submit_time']  = ['between', [$params['start_time'], $params['end_time']]];
+    }
+    $data = Db::table('techhanding_list')->where($conditions)->order('submit_time', 'desc')->select();
+    foreach ($data as $key => $value) {
+      // 获取区域、教室信息
+      $room_info = Db::table('rooms')->where('id', $value['room_id'])->find();
+      $area_info = Db::table('areas')->where('id', $value['area_id'])->find();
+      $data[$key]['area_name'] = $area_info['area_name'];
+      $data[$key]['room_name'] = $room_info['room_name'];
+
+      // 获取设备信息
+      $et_info = Db::table('equipments')->where('id', $value['equipment_id'])->find();
+      $et_info['et_type'] = Db::table('equipment_class')->where('id', $et_info['class_id'])->value('equipment_name');
+      $data[$key]['et_info'] = $et_info;
+
+      // 获取故障列表
+      $fault_list = Db::table('techhanding_fault')->where('techhanding_id', $value['id'])->select();
+      foreach ($fault_list as $sub_key => $sub_value) {
+        $fault_list[$sub_key]['fault_name'] = Db::table('faults_class')->where('id', $sub_value['fault_id'])->value('fault_name');
+      }
+      $data[$key]['fault_list'] = $fault_list;
+    }
+    return $this->formatData('ok', $data);
   }
 
 
